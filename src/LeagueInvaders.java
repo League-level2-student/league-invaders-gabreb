7db47c3774de4fd26fa6b9944241a67be2041c40
gabreb <62906858+gabreb@users.noreply.github.com>
2020-05-13 13:33:44 -0700

    HI

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,28 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	GamePanel gamepanel;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+public static void main(String[] args) {
+	LeagueInvaders vader = new LeagueInvaders();
+	vader.setup();
 }
+LeagueInvaders() {
+	frame = new JFrame();
+	gamepanel = new GamePanel();
+}
+void setup() {
+	frame.add(gamepanel);
+	frame.setSize(WIDTH,HEIGHT);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	
+}
+}
+
+
+
+
+
